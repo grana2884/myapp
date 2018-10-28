c138414e7667c8596efa611df275be4583e17da2
Gaurav S Rana <gauravrana.dce@gmail.com>
2018-10-28 20:07:34 +0530

    first commit

---
+++ myprofile/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, 'base.html')
+
+
+def create_profile():
+    pass
+
+
+def update_profile():
+    pass
+
+
+def delete_profile():
+    pass
+
+
+def login_profile():
+    pass
+
+
+def logout_profile():
+    pass
+
+
+def perm_profile():
+    pass
+
+
+def change_password():
+    pass
+
+
+def reset_password():
+    pass
