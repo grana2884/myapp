5a6cb90af9eeaa5e3395585e59512474a1df3044
Gaurav S Rana <gauravrana.dce@gmail.com>
2018-10-28 21:08:02 +0530

    html update

--- myprofile/views.py
+++ myprofile/views.py
@@ -1,12 +1,35 @@
 from django.shortcuts import render
+from myprofile.forms import ProfileForm, UserForm
 
 
 def index(request):
     return render(request, 'base.html')
 
 
-def create_profile():
-    pass
+def create_profile(request):
+    registered = False
+
+    if request.method == 'POST':
+        user_form = UserForm(data=request.POST)
+        profile_form = ProfileForm(data=request.POST)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user = user_form.save()
+            user.set_password(user.password)
+            user.save()
+
+            profile = profile_form.save(commit=False)
+            profile.user = user
+            profile.save()
+            registered = True
+        else:
+            print(user_form.errors, profile_form.errors)
+    else:
+        user_form = UserForm()
+        profile_form = ProfileForm()
+
+    return render(request, 'base.html', {'user_form': user_form, 'profile_form': profile_form, 'registered': registered}
+                  )
 
 
 def update_profile():
