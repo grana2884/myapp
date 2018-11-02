f825f56a7329891130fa1ea4417fe8ddbd2bdda4
Gaurav S Rana <gauravrana.dce@gmail.com>
2018-11-03 02:01:49 +0530

    login

--- myprofile/views.py
+++ myprofile/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout, authenticate
-from django.http import HttpResponseRedirect
 from myprofile.forms import ProfileForm, UserForm
 
 
